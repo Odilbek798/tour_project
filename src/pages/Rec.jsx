dc62be28aa2216b06ff20e5b887a2221214f80c5
Odilbek798 <odilbekiskandaroviskandarov@gmail.com>
2026-02-17 16:16:07 +0500

    Textcard

--- src/pages/Rec.jsx
+++ src/pages/Rec.jsx
@@ -1,11 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Cards from "../components/Cards/Cards";
-
+import TextCard from "../components/TextCard";
+import axios from 'axios';
+import Header from "../components/Header/Header";
 
 function Rec() {
+  const [coutry, setCountry] = useState([])
+
+  const getTours = async () => {
+    const res = await axios.get("https://backend-iobr.onrender.com/api/countries")
+    setCountry(res.data)
+  }
+
+  useEffect(() => {
+    getTours()
+  }, [])
   return(
-    <div>
-      yuio
+    <div className="rec">
+      <Header/>
+      <div className="cards">
+          {coutry.map((c) => (
+      <TextCard
+        key={c._id}
+        text={c.description}
+        title={c.name}
+        route={`/tickets/${c._id}`}
+        tags="Ko'rish"
+      />
+    ))}
+      </div>
     </div>
   );
   
