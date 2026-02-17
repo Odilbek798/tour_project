b009aca14fbcf6aa891a6881dcf754f2a29af4b8
Odilbek798 <odilbekiskandaroviskandarov@gmail.com>
2026-02-17 16:48:50 +0500

    ertgvghj

--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -4,12 +4,20 @@ import { Link } from 'react-router-dom'
 const TextCard = ({ title, text, tags, route, image }) => {
   return (
     <div>
-      <div className="card">
+      <div className="card"
+      style={{
+        backgroundImage: "url('/view.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+      >
+        <div className="cardT">
         <h1>{title}</h1>
         <p>{text}</p>
-
         <Link to={route}>
           <button >{tags}</button></Link>
+          </div>
       </div>
     </div>
   )
